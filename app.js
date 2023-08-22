d46b9001e92b00d0ad2195f0b801af979fd7abe0
Fabio <clonefabster7@yahoo.com>
2023-08-21 18:10:12 -0700

    Practiced fetching and unlocking

---
+++ app.js
@@ -0,0 +1,25 @@
+// console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
+ 
+const emailRef = document.querySelector(".email");
+
+
+// 1. Then
+fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
+response.json().then(data => {
+    emailRef.innerHTML = data.email;
+})
+})
+
+//Cleaner/More maintainable
+
+fetch ("https://jsonplaceholder.typicode.com/users/1")
+.then((response) => {
+    return response.json();
+})
+.then((data) => {
+    console.log(data);
+    emailRef.innerHTML = data.email
+});
+
+
+// 2. Async/Await
