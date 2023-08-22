3fc7f823dce163d81711fa8c622e326af82b7f8e
Fabio <clonefabster7@yahoo.com>
2023-08-22 15:38:27 -0700

    Async and creating promises

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
  
-const emailRef = document.querySelector(".email");
+ const emailRef = document.querySelector(".email");
 
 
-// 1. Then
+/* 1. Then
 fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
 response.json().then(data => {
     emailRef.innerHTML = data.email;
@@ -21,5 +21,33 @@ fetch ("https://jsonplaceholder.typicode.com/users/1")
     emailRef.innerHTML = data.email
 });
 
+*/
 
-// 2. Async/Await
+//  2. Async/Await
+
+async function main() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+const data = await response.json()
+console.log(data)
+emailRef.innerHTML = data.email
+}
+
+main();
+
+//Creating promise
+
+ function getSubscriptionStatus () {
+    return new Promise ((resolve, reject) => {
+        resolve("VIP")
+
+    })
+ }
+
+ //1. Then
+ getSubscriptionStatus().then(response =>console.log(response))
+
+//2. Async/Await
+async function main () {
+    console.log(await getSubscriptionStatus())
+}
+main ();
